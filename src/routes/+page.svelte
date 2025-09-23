7bb6e5ade2f9e50c249bae214fe513cc0ea7a9dd
nicbat <nbatch@outlook.com>
2025-09-22 17:00:15 -0700

    small bio update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="flex flex-col md:flex-row-reverse justify-center items-center w-full">
     <div class="w-full md:w-1/2 mb-6 md:mb-0 md:ml-10">
         <h1 class="text-center">Hey, I'm Nicholas</h1>
-        I'm an incoming Master's Student at the University of Washington studying Computer
+        I'm a Master's Student at the University of Washington studying Computer
         Science. I currently work on Proactive and Agentic LLMs at the <a
             href="https://netlab.cs.washington.edu/"
             target="_blank">Mobile Intelligence Lab</a>
@@ -11,13 +11,14 @@
              <!-- and <a href="https://jedels.github.io/" target="_blank">Jacob Lee Edelson</a> -->
               under <a href="https://homes.cs.washington.edu/~gshyam/" target="_blank"
             >Shyam Gollakota</a>. 
-        I've just finished my Summer Internship at <a href="https://careers.roblox.com/" target="_blank">Roblox</a> in sunny San Mateo.
-        This fall I'll be TAing for CSE 461: Networks.
+        I'm also a TA for <a href="https://courses.cs.washington.edu/courses/cse461/25au/">CSE 461: Networks</a>.
+        Starting August 2026 I'll be working fulltime at <a href="https://careers.roblox.com/" target="_blank">Roblox</a> in sunny San Mateo.
         <br/><br/>
         Outside of programming, I love studying languages, tinkering with my computer, and exploring the west coast.
         <br/><br/>
         You can contact me at <a href="mailto:nicbat@cs.washington.edu">nicbat@cs.washington.edu</a>.
     </div>
     <!-- TODO lower quality of photo to load it faster - or move this to static or something, would that be faster? -->
+     <!-- TODO honestly see how small the photo ends up being, then just compress it to that size? -->
     <img src="home_photo.png" alt="Nicholas Batchelder" class="w-full md:w-1/2 rounded-lg" />
 </div>
